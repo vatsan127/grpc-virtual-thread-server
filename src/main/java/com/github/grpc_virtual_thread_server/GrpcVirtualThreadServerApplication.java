10aa7752881f40268fef8853522e8a1200e84bae
Srivatsan N <vatsan127@gmail.com>
2026-07-03 21:39:38 +0530

    first commit

---
+++ src/main/java/com/github/grpc_virtual_thread_server/GrpcVirtualThreadServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.grpc_virtual_thread_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcVirtualThreadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcVirtualThreadServerApplication.class, args);
+	}
+
+}
